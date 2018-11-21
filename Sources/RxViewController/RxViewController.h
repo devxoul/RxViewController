9b8b45ad876ece373b3b9bd5d5d829ed27ffd641
Sourav Chandra <saurav.chandra1992@live.com>
2018-11-21 16:59:34 +0530

    Updated to support carthage

---
+++ Sources/RxViewController/RxViewController.h
@@ -0,0 +1,19 @@
+//
+//  RxViewController.h
+//  RxViewController
+//
+//  Created by Sourav Chandra on 21/11/18.
+//  Copyright © 2018 Sourav Chandra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxViewController.
+FOUNDATION_EXPORT double RxViewControllerVersionNumber;
+
+//! Project version string for RxViewController.
+FOUNDATION_EXPORT const unsigned char RxViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxViewController/PublicHeader.h>
+
+

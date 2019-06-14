d780a2c6172e95c3d3e9fc1350c96516355dd45a
Suyeol Jeon <devxoul@gmail.com>
2019-06-14 16:39:28 +0900

    Use RxSwift 5.0

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     .library(name: "RxViewController", targets: ["RxViewController"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.0.0")),
-    .package(url: "https://github.com/devxoul/RxExpect.git", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+    .package(url: "https://github.com/devxoul/RxExpect.git", .upToNextMajor(from: "2.0.0")),
   ],
   targets: [
     .target(name: "RxViewController", dependencies: ["RxSwift", "RxCocoa"]),
